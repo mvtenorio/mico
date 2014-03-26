2d4f32f3bed53a1d5d77008e459997eb2368a124
Paulo Henrique Domiciano <phdomiciano@dm4brasil.com>
2014-03-26 18:38:09 -0300

    Corrigindo caminho do css e js com asset

--- app/views/layouts/partials/head.blade.php
+++ app/views/layouts/partials/head.blade.php
@@ -1,6 +1,6 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
-<link rel="stylesheet" href="{{ url('css/application.css') }}">
+<link rel="stylesheet" href="{{ asset('css/application.css') }}">
 @yield('css')
 
-<script src="{{ url('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js') }}"></script>
+<script src="{{ asset('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js') }}"></script>
